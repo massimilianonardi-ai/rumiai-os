0bc04881c0880756b13c5d43d85a1f48363bba78
massimilianonardi-ai <massimiliano.nardi.ai@gmail.com>
2026-09-04 23:51:38 +0200

    optimized

--- lib/sh/core.lib.sh
+++ lib/sh/core.lib.sh
@@ -189,7 +189,6 @@ shell()
 
   : "${m_SHELL_NAME:=${SHELL##*/}}"
   printf -- '%s\n'  "$(tput setaf 2)$(tput bold)${m_SHELL_NAME} $(tput setaf 7)${m_OS_NAME}$(tput sgr0)" >&2
-  export m_SHELL_PS1="$(tput setaf 7)$(tput bold)${m_OS_NAME}:${m_SHELL_NAME}$(tput sgr0) $(tput setaf 2)\u$(tput setaf 7)@$(tput setaf 2)\h$(tput setaf 7):$(tput setaf 2)\w$(tput setaf 4) \$$(tput sgr0) "
 
   : "${m_SHELL_EXT:=}"
   export m_SHELL_EXT
