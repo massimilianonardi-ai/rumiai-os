16e1eb638771972ea4dfd53e6429a9e583c6e6fc
massimilianonardi-ai <massimiliano.nardi.ai@gmail.com>
2026-09-18 21:10:30 +0200

    Add provider selector resolution

--- lib/sys/sh/pkg-provider.lib.sh
+++ lib/sys/sh/pkg-provider.lib.sh
@@ -248,6 +248,171 @@ _pkg_provider_bind()
   fi
 }
 
+
+_pkg_provider_concrete_parse()
+{
+  [ "$#" -eq 1 ] || return 2
+  pkg_provider_concrete=$1
+  pkg_provider_concrete_left=$pkg_provider_concrete
+  pkg_provider_concrete_osarch=
+
+  case "$pkg_provider_concrete_left" in
+    *!*)
+      pkg_provider_concrete_osarch=${pkg_provider_concrete_left##*!}
+      pkg_provider_concrete_left=${pkg_provider_concrete_left%!"$pkg_provider_concrete_osarch"}
+      case "$pkg_provider_concrete_left" in *!*) return 1 ;; esac
+      _pkg_provider_osarch_valid "$pkg_provider_concrete_osarch" || return 1
+      ;;
+  esac
+
+  case "$pkg_provider_concrete_left" in
+    *@*)
+      pkg_provider_concrete_version=${pkg_provider_concrete_left##*@}
+      pkg_provider_concrete_pkg=${pkg_provider_concrete_left%@"$pkg_provider_concrete_version"}
+      case "$pkg_provider_concrete_pkg" in *@*) return 1 ;; esac
+      ;;
+    *)
+      return 1
+      ;;
+  esac
+
+  _pkg_provider_name_valid "$pkg_provider_concrete_pkg" || return 1
+  _pkg_provider_version_valid "$pkg_provider_concrete_version"
+}
+
+_pkg_provider_resolved_validate()
+{
+  [ "$#" -eq 4 ] || return 2
+  pkg_provider_resolved_name=$1
+  pkg_provider_expected_pkg=$2
+  pkg_provider_expected_version=$3
+  pkg_provider_consumer_osarch=$4
+
+  _pkg_provider_concrete_parse "$pkg_provider_resolved_name" || return 1
+  [ "$pkg_provider_concrete_pkg" = "$pkg_provider_expected_pkg" ] || return 1
+  if [ -n "$pkg_provider_expected_version" ]
+  then
+    [ "$pkg_provider_concrete_version" = "$pkg_provider_expected_version" ] || return 1
+  fi
+
+  if [ -n "$pkg_provider_consumer_osarch" ]
+  then
+    [ -z "$pkg_provider_concrete_osarch" ] || [ "$pkg_provider_concrete_osarch" = "$pkg_provider_consumer_osarch" ] || return 1
+  else
+    [ -z "$pkg_provider_concrete_osarch" ] || return 1
+  fi
+
+  [ -d "$m_PKG_DIR/$pkg_provider_resolved_name" ] && [ ! -L "$m_PKG_DIR/$pkg_provider_resolved_name" ]
+}
+
+_pkg_provider_selector_current()
+{
+  [ "$#" -eq 3 ] || return 2
+  pkg_provider_current_pkg=$1
+  pkg_provider_current_osarch=$2
+  pkg_provider_allow_generic=$3
+  pkg_provider_current_name=
+
+  if [ -n "$pkg_provider_current_osarch" ]
+  then
+    pkg_provider_current_path="$m_PKG_DIR/$pkg_provider_current_pkg!$pkg_provider_current_osarch"
+    if [ -L "$pkg_provider_current_path" ]
+    then
+      pkg_provider_current_name="$(command -p -- readlink "$pkg_provider_current_path")" || return 1
+    elif [ -e "$pkg_provider_current_path" ]
+    then
+      return 1
+    elif [ "$pkg_provider_allow_generic" -eq 1 ]
+    then
+      pkg_provider_current_path="$m_PKG_DIR/$pkg_provider_current_pkg"
+      [ -L "$pkg_provider_current_path" ] || {
+        [ ! -e "$pkg_provider_current_path" ] || return 1
+        return 1
+      }
+      pkg_provider_current_name="$(command -p -- readlink "$pkg_provider_current_path")" || return 1
+    else
+      return 1
+    fi
+  else
+    pkg_provider_current_path="$m_PKG_DIR/$pkg_provider_current_pkg"
+    [ -L "$pkg_provider_current_path" ] || {
+      [ ! -e "$pkg_provider_current_path" ] || return 1
+      return 1
+    }
+    pkg_provider_current_name="$(command -p -- readlink "$pkg_provider_current_path")" || return 1
+  fi
+
+  case "$pkg_provider_current_name" in "" | */*) return 1 ;; esac
+}
+
+pkg_provider_selector_resolve()
+(
+  [ "$#" -eq 1 ] || [ "$#" -eq 2 ] || return 2
+  pkg_provider_resolve_selector=$1
+  pkg_provider_resolve_consumer_osarch=${2-}
+
+  if [ -n "$pkg_provider_resolve_consumer_osarch" ]
+  then
+    _pkg_provider_osarch_valid "$pkg_provider_resolve_consumer_osarch" || return 2
+  fi
+
+  _pkg_provider_selector_validate "$pkg_provider_resolve_selector" || return 2
+  pkg_provider_resolve_pkg=$pkg_provider_selector_pkg
+  pkg_provider_resolve_version=$pkg_provider_selector_version
+  pkg_provider_resolve_selector_osarch=$pkg_provider_selector_osarch
+
+  if [ -n "$pkg_provider_resolve_selector_osarch" ]
+  then
+    [ -n "$pkg_provider_resolve_consumer_osarch" ] || return 1
+    [ "$pkg_provider_resolve_selector_osarch" = "$pkg_provider_resolve_consumer_osarch" ] || return 1
+    pkg_provider_resolve_osarch=$pkg_provider_resolve_selector_osarch
+    pkg_provider_resolve_allow_generic=0
+  else
+    pkg_provider_resolve_osarch=$pkg_provider_resolve_consumer_osarch
+    pkg_provider_resolve_allow_generic=1
+  fi
+
+  if [ -n "$pkg_provider_resolve_version" ]
+  then
+    if [ -n "$pkg_provider_resolve_osarch" ]
+    then
+      pkg_provider_resolved="$pkg_provider_resolve_pkg@$pkg_provider_resolve_version!$pkg_provider_resolve_osarch"
+      if [ ! -d "$m_PKG_DIR/$pkg_provider_resolved" ] || [ -L "$m_PKG_DIR/$pkg_provider_resolved" ]
+      then
+        [ "$pkg_provider_resolve_allow_generic" -eq 1 ] || return 1
+        pkg_provider_resolved="$pkg_provider_resolve_pkg@$pkg_provider_resolve_version"
+      fi
+    else
+      pkg_provider_resolved="$pkg_provider_resolve_pkg@$pkg_provider_resolve_version"
+    fi
+  else
+    _pkg_provider_selector_current "$pkg_provider_resolve_pkg" "$pkg_provider_resolve_osarch" "$pkg_provider_resolve_allow_generic" || return 1
+    pkg_provider_resolved=$pkg_provider_current_name
+  fi
+
+  _pkg_provider_resolved_validate "$pkg_provider_resolved" "$pkg_provider_resolve_pkg" "$pkg_provider_resolve_version" "$pkg_provider_resolve_consumer_osarch" || return 1
+  printf -- '%s\n' "$pkg_provider_resolved"
+)
+
+pkg_provider_effective_selector()
+(
+  [ "$#" -eq 2 ] || return 2
+  pkg_provider_effective_consumer=$1
+  pkg_provider_effective_facility=$2
+
+  _pkg_provider_binding_file "$pkg_provider_effective_consumer" "$pkg_provider_effective_facility" || return $?
+  pkg_provider_effective_binding=$pkg_provider_config_file
+
+  if [ -e "$pkg_provider_effective_binding" ] || [ -L "$pkg_provider_effective_binding" ]
+  then
+    _pkg_provider_config_query "$pkg_provider_effective_binding"
+    return $?
+  fi
+
+  _pkg_provider_default_file "$pkg_provider_effective_facility" || return $?
+  _pkg_provider_config_query "$pkg_provider_config_file"
+)
+
 pkg_provider()
 (
   [ "$#" -ge 1 ] || return 2
