045e5422d8e058e6611f580251b02d48c36acac1
massimilianonardi-ai <massimiliano.nardi.ai@gmail.com>
2026-09-22 22:06:07 +0200

    Add pkg requirement subcommand library

---
+++ lib/sys/sh/pkg/pkg-requirement.lib.sh
@@ -0,0 +1,18 @@
+. "$m_LIB_DIR/sys/sh/pkg/facility/pkg-dependency.lib.sh"
+
+pkg_requirement()
+(
+  [ "$#" -ge 1 ] || return 2
+  pkg_requirement_action=$1
+  shift
+
+  case "$pkg_requirement_action" in
+    resolve)
+      [ "$#" -ge 2 ] || return 2
+      pkg_dependency_default_resolve "$@"
+      ;;
+    *)
+      return 2
+      ;;
+  esac
+)
