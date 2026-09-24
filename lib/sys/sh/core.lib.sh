0868df747acfb7099f68625188d18f0c3453b70d
massimilianonardi-ai <massimiliano.nardi.ai@gmail.com>
2026-09-24 22:47:42 +0200

    fatal support return

--- lib/sys/sh/core.lib.sh
+++ lib/sys/sh/core.lib.sh
@@ -65,6 +65,7 @@ log()
 
 fatal()
 {
+  case "$1" in exit) EXIT_COMMAND="exit";; return) EXIT_COMMAND="return"; shift;; *) EXIT_COMMAND="exit"; shift;; esac
   case "$1" in *[!0-9]* | "") EXIT_CODE="1";; "0" | "00" | "000" | ????*) EXIT_CODE="1"; shift;; *) EXIT_CODE="$1"; shift; [ "$EXIT_CODE" -gt "255" ] && EXIT_CODE="1";; esac
 
   log fatal "$@" || log_base_print "$@"
